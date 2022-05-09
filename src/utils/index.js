bc56904f1e5b8495fe5c4914e642f580bafe5ed3
lucas-andrad <lafdesouza2002@outlook.com>
2022-05-09 20:44:37 -0300

    feat: Add index for utils

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+const getExpirationDateFromLine = require("./getExpirationDateFromLine");
+const getValueFromLine = require("./getValueFromLine");
+const typeBoletoVerifier = require("./typeBoletoVerifier");
+
+module.exports = {
+    getValueFromLine,
+    getExpirationDateFromLine,
+    typeBoletoVerifier,
+};
