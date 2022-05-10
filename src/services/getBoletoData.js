baed0d4ebfb1f8788302567a124f5be256f082b6
lucas-andrad <lafdesouza2002@outlook.com>
2022-05-10 00:56:16 -0300

    fix: Corrige nome do objeto no json

--- src/services/getBoletoData.js
+++ src/services/getBoletoData.js
@@ -1,11 +1,10 @@
 const { getValueFromLine, getExpirationDateFromLine } = require("../utils");
 
 const getBoletoData = (line, typeOfBoleto) => {
-    console.log(typeOfBoleto)
     const value = getValueFromLine(line, typeOfBoleto);
     const expirationDate = getExpirationDateFromLine(line, typeOfBoleto);
     return {
-        value,
+        amount: value,
         expirationDate,
         barCode: line,
     };
