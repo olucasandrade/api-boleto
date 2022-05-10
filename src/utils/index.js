52a07dfcaabc29f47e3c2b18a2816c840264ebc7
lucas-andrad <lafdesouza2002@outlook.com>
2022-05-10 00:56:32 -0300

    feat: Adiciona novos utils ao index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,17 @@
 const getExpirationDateFromLine = require("./getExpirationDateFromLine");
 const getValueFromLine = require("./getValueFromLine");
+const hasOnlyNumbers = require("./hasOnlyNumbers");
+const hasValidDigit = require("./hasValidDigit");
+const hasValidLength = require("./hasValidLength");
+const sumAllFigures = require("./getDVsByFigures");
 const typeBoletoVerifier = require("./typeBoletoVerifier");
 
 module.exports = {
     getValueFromLine,
     getExpirationDateFromLine,
     typeBoletoVerifier,
+    hasOnlyNumbers,
+    hasValidDigit,
+    hasValidLength,
+    sumAllFigures,
 };
