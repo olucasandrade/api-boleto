1f820544d633f1a149672f76406992b803da07b7
lucas-andrad <lafdesouza2002@outlook.com>
2022-05-10 00:54:57 -0300

    feat: Adiciona verificadores no validateData

--- src/middlewares/validateData.js
+++ src/middlewares/validateData.js
@@ -1,7 +1,12 @@
-const { typeBoletoVerifier } = require("../utils");
+const { typeBoletoVerifier, hasOnlyNumbers, hasValidLength, hasValidDigit } = require("../utils");
 
 const validateData = (req, res, next) => {
     typeBoletoVerifier(req)
+    const { typeOfBoleto, params } = req;
+    const { line } = params;
+    if (!hasOnlyNumbers(line)) res.status(400).json({ message: "A linha digitável deve possuir apenas números." });
+    if (!hasValidLength(line)) res.status(400).json({ message: "A linha digitável deve conter 47 ou 48 dígitos." });
+    if (!hasValidDigit(line, typeOfBoleto)) res.status(400).json({ message: "Dígito verificador inválido!" });
     next();
 };
 
