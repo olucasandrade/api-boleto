c3d603cfc186c132ff34a845f0092efbb2eb47ed
lucas-andrad <lafdesouza2002@outlook.com>
2022-05-10 00:56:00 -0300

    fix: Remove console.log

--- src/utils/getDVsByFigures.js
+++ src/utils/getDVsByFigures.js
@@ -3,7 +3,6 @@ const cutValue = require("./cutValue");
 const getDVsByFigures = (field1, field2, field3) => {
     let index = 0;
     const sums = [field1, field2, field3].map(field => {
-        console.log(field.slice(0, -1))
         const valuesToSum = field.slice(0, -1).split('');
         const total = valuesToSum.map(n => Number(n)).reduce((acc, value, i) => {
             const valueToMultiply = index % 2 === 0 ? 2 : 1;
@@ -16,7 +15,6 @@ const getDVsByFigures = (field1, field2, field3) => {
         }, 0);
         return total;
     });
-    console.log([])
     return sums.map(sum => {
         const mod = sum % 10;
         return 10 - mod;
